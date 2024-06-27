7e76b482ccc287f4adf2172d3eb87d0eeb9344be
Anugrah Naranatt <pnanugrah@gmail.com>
2024-06-27 14:41:41 -0700

    added gitignore

--- test.py
+++ test.py
@@ -1,12 +1,26 @@
 from whisper_mic import WhisperMic
 
-mic = WhisperMic()
-result = mic.listen()
+# mic = WhisperMic(save_file=True,pause=1, device="cuda", english=True, energy=500 )
+# wakewords = ["Aaron", "aaron"]
+# while True:
+#     result = mic.listen()
+#
+#     wakewordInString = [ele for ele in wakewords if(ele in result)]
+#     if wakewordInString:
+#         print("sent to chatgpt")
+#     else:
+#         print("not sent")
+#     print(result)
 
-wakewords = ["Aaron", "aaron"]
-res = [ele for ele in wakewords if(ele in result)]
-if res:
-    print("sent to chatgpt")
-else:
-    print("not sent")
-print(result)
+
+def speechToText():
+    mic = WhisperMic(save_file=True, pause=1, device="cuda", english=True, energy=500 )
+    wakewords = ["Aaron", "aaron"]
+    
+    userCommand = mic.listen()
+    wakewordInString = [ele for ele in wakewords if(ele in userCommand)]
+    if wakewordInString:
+        print("sent to chatgpt")
+    else:
+        print("not sent")
+    print(userCommand)
